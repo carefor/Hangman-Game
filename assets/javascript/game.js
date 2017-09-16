46ac5efcff7d60986d5e0e32574f8a3d7d7f6b80
Caroline Forsyth <uncforsyth@gmail.com>
2017-09-16 15:22:36 -0400

    tons of js fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,130 +2,119 @@
 
 //declaring variables
 var wordList = [
-["A", "R", "G", "E", "N", "T", "I", "N", "A"],
-  ["B", "E", "L", "G", "I", "U", "M"],
-  ["C", "Y", "P", "R", "U", "S"],
-  ["F", "I", "N", "L", "A", "N", "D"],
-  ["H", "U", "N", "G", "A", "R", "Y"],
-  ["I", "N", "D", "O", "N", "E", "S", "I", "A"],
-  ["J", "O", "R", "D", "A", "N"],
-  ["K", "E", "N", "Y", "A"],
-  ["K", "Y", "R", "G", "Y", "Z", "S", "T", "A", "N"],
-  ["L", "U", "X", "E", "M", "B", "O", "U", "R", "G"],
-  ["M", "A", "L", "T", "A"],
-  ["M", "O", "Z", "A", "M", "B", "I", "Q", "U", "E"],
-  ["N", "E", "T", "H", "E", "R", "L", "A", "N", "D", "S"],
-  ["S", "W", "I", "T", "Z", "E", "R", "L", "A", "N", "D"],
-  ["S", "L", "O", "V", "E", "N", "I", "A"],
-  ["T", "H", "A", "I", "L", "A", "N", "D"],
-  ["T", "U", "R", "K", "E", "Y"]
+    ["A", "R", "G", "E", "N", "T", "I", "N", "A"],
+    ["B", "E", "L", "G", "I", "U", "M"],
+    ["C", "Y", "P", "R", "U", "S"],
+    ["F", "I", "N", "L", "A", "N", "D"],
+    ["H", "U", "N", "G", "A", "R", "Y"],
+    ["I", "N", "D", "O", "N", "E", "S", "I", "A"],
+    ["J", "O", "R", "D", "A", "N"],
+    ["K", "E", "N", "Y", "A"],
+    ["K", "Y", "R", "G", "Y", "Z", "S", "T", "A", "N"],
+    ["L", "U", "X", "E", "M", "B", "O", "U", "R", "G"],
+    ["M", "A", "L", "T", "A"],
+    ["M", "O", "Z", "A", "M", "B", "I", "Q", "U", "E"],
+    ["N", "E", "T", "H", "E", "R", "L", "A", "N", "D", "S"],
+    ["S", "W", "I", "T", "Z", "E", "R", "L", "A", "N", "D"],
+    ["S", "L", "O", "V", "E", "N", "I", "A"],
+    ["T", "H", "A", "I", "L", "A", "N", "D"],
+    ["T", "U", "R", "K", "E", "Y"]
 ]
 
-var randomIndex = Math.floor( Math.random()*(wordList.length - 1) );
+var randomIndex = Math.floor(Math.random() * (wordList.length - 1));
 var chosenWord = wordList[randomIndex];
 var dashedWord = new Array(chosenWord.length);
+var isSame = true;
 
 var activeWord = document.getElementById("active-word");
 var resultsArea = document.getElementById("results");
 
 var keyChar = "";
 
-var match = false;
-var gameWon = false;
-
 var mistakeCounter = 7;
 var winsCounter = 0;
 
 //replaces each letter (index) with a dash
-for (var i = 0; i < dashedWord.length; i++){
-   dashedWord[i] = "_ ";
+for (var i = 0; i < dashedWord.length; i++) {
+    dashedWord[i] = "_ ";
 }
 
 //prints the dashed word on the screen
-   function displayWord() {
-      for (var i = 0; i < dashedWord.length; i++){
+function displayWord() {
         activeWord.innerHTML = dashedWord.join("");
         console.log("The word is " + chosenWord.join(""));
-      }
-  }
-    displayWord();
+}
+displayWord();
 
 //compares if key pressed is in the word
- function compareLetter(keypress) {
-        var keyChar = keypress;
-        var isCorrectGuess = false;
-        if(chosenWord.indexOf(keyChar) === -1) {
-          return isCorrectGuess;
-        } else {
-          return chosenWord.indexOf(keyChar);
-        }
-        }
+function compareLetter(keypress) {
+    var keyChar = keypress;
+    var isCorrectGuess = false;
+    if (chosenWord.indexOf(keyChar) === -1) {
+        return isCorrectGuess;
+    } else {
+        return chosenWord.indexOf(keyChar);
+    }
+}
 
-       //if key pressed is in word it prints it to the screen
-      function letterMatches(attempt, keyChar) {
-        dashedWord[attempt] = keyChar;
-        activeWord.innerText = dashedWord.join("");
-       }
-       
+//if key pressed is in word it prints it to the screen
+function letterMatches(attempt, keyChar) {
+    for (var i = 0; i < dashedWord.length; i++) {
+    dashedWord[attempt] = keyChar;
+    activeWord.innerText = dashedWord.join("");
+  }
+}
 
-      //wrong guesses result in letter printed to screen and wrong guess counter decreased
-    function wrongGuess() {
-      if(match = false){
-      keyChar = event.key.toUpperCase();
-      var guessedLetter = document.getElementById("guessed");
-      var repLetter = document.createTextNode(keyChar);
-      guessedLetter.appendChild(repLetter); 
-      mistakeCounter--;
-      var guessCounter = document.getElementById("guess-counter");
-      var guessesLeft = document.createTextNode(mistakeCounter);
-      guessCounter.appendChild(guessesLeft);
-       }
-     }
+//wrong guesses result in letter printed to screen and wrong guess counter decreased
+function wrongGuess(keyChar) {
+    var guessesDisplay = document.getElementById("guessed");
+    var repLetter = document.createTextNode(keyChar);
+    guessesDisplay.appendChild(repLetter);
+    mistakeCounter--;
+    console.log("guesses left: ", mistakeCounter, ";", "wrong letter: ", keyChar);
+    var guessCounter = document.getElementById("guess-counter");
+    guessCounter.innerHTML = mistakeCounter.toString();
+}
 
-//reads if all dashes have been replaced with letters and so if game is ongoing
-//function gameOngoing() {
-  //    for (var i = 0; i < dashedWord.length; i++){
-    //     if(dashedWord[i] === "_ "){
-      //   gameWon = false;
-        // }
-         //else if(dashedWord[i] !== "_ "){
-          //gameWon = true;
-         //}
-      //}
-    //}
+//if run out of guesses, game lost message displayed
+function gameLost() {
+  if (mistakeCounter === 0) {
+    console.log("Too many mistakes were made. Game over");
+    alert("Too many tries, you lose!");
+    var resultsArea = document.getElementById("results");
+    var loseText = "Hope you bought travel insurance.. You lost.";
+    resultsArea.innerHTML = loseText;
+  }
+}
 
 //if win, displays text and increases wins counter
-//function winningGame() {
-  // if(gameWon = true){
-    //  var winText = document.createTextNode("Bon voyage! You win!! The answer is indeed " + chosenWord.join(""));
-      //resultsArea.appendChild(winText);
-     // winsCounter++;
-     // var wins = document.getElementById("wins");
-     // var displayWins = document.createTextNode(winsCounter);
-     // wins.appendChild(displayWins);
- // }
- //}
-
-//if run out of guesses, 'you lost' message displayed
-function losingGame() {
-   if(mistakeCounter === 0) {
-      resultsArea.innerHTML = "Hope you bought travel insurance.. You lost.";
+function gameWon() {
+var allMatch = true;
+  for (var i = 0; i < dashedWord.length; i++){
+    if(dashedWord[i] === "_ "){
+      allMatch = false;
     }
   }
+  if(allMatch){
+    console.log("Game won");
+    winsCounter++;
+    var resultsArea = document.getElementById("results");
+    var winText = "Bon voyage! You win!! The answer is indeed " + chosenWord.join("");
+    resultsArea.innerHTML = winText;
+    var winsDisplay = document.getElementById("wins");
+    winsDisplay.innerHTML = winsCounter.toString();
+   }
+}
 
 //recognizes key pressed and runs functions
-    document.onkeypress = function (event) {
-      keyChar = event.key.toUpperCase();
-          var attempt = compareLetter(keyChar);
-          if(attempt !== false){
-            letterMatches(attempt, keyChar);
-          } else {
-          wrongGuess();
-          }
-          //letterMatches();
-          
-          //gameOngoing() 
-         //winningGame();
-          }
-        
-
+document.onkeypress = function(event) {
+    keyChar = event.key.toUpperCase();
+    var attempt = compareLetter(keyChar);
+    if (attempt !== false) {
+        letterMatches(attempt, keyChar);
+    } else {
+        wrongGuess(keyChar);
+    }
+    gameLost();
+    gameWon();
+}
